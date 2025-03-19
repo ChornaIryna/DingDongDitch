e56a975e34903a67f04aa550e15c4426b4a481ff
Iryna <iryna.chorna@hotmail.com>
2025-03-19 16:49:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DingDongDitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nq = Console.ReadLine().Split(' ');
+            int n = int.Parse(nq[0]);
+            int q = int.Parse(nq[1]);
+
+            int[] angerValues = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] ambitions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Array.Sort(angerValues);
+
+            for (int i = 0; i < q; i++)
+            {
+                long sum = 0;
+                for (int j = 0; j < ambitions[i]; j++)
+                {
+                    sum += angerValues[j];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
